770fc51dbf154914a2fd877aaa33f3363c37f9db
Ranjeet <kiranjeetkr80@gmail.com>
2025-10-30 16:25:20 +0530

    implementing splash screen

--- app/src/main/java/com/example/financialapp/Activities/IntroActivity/IntroActivity.kt
+++ app/src/main/java/com/example/financialapp/Activities/IntroActivity/IntroActivity.kt
@@ -1,21 +1,24 @@
 package com.example.financialapp.Activities.IntroActivity
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.financialapp.Activities.IntroActivity.screens.IntroScreen
+import com.example.financialapp.MainActivity
 import com.example.financialapp.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            IntroScreen (onStartClick = {
+                startActivity(Intent(this, MainActivity::class.java))
+            })
         }
     }
 }
